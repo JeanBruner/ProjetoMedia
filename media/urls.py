bb06a49cd9869c82efc7b80738a6a29c60b24899
JeanBruner <jeanbruner2@gmail.com>
2019-02-08 15:38:03 -0200

    As Views foram alteradas de Funções para Classes, portanto as urls das mesmas também tiveram que ser modificadas

--- media/urls.py
+++ media/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
-from django.views.generic.base import TemplateView
-
+from django.views.generic.base import RedirectView
+from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('', views.notas, name='notas'),
-    path('', TemplateView.as_view(template_name='list.html'), name='home'),
-    path('admin/', admin.site.urls)
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='media/login.html',
+        ),
+        name="login"
+    ),
+    path('cadastro/', views.Cadastro.as_view(), name='cadastro'),
+    path('', RedirectView.as_view(url=reverse_lazy('mostrar'))),
+    path('admin/', admin.site.urls),
+    path('alunos/', views.Notas.as_view(), name="mostrar")
 ]

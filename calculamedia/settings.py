fe315ae8f3255ff374370e9555576cea320a217d
JeanBruner <jeanbruner2@gmail.com>
2019-02-08 15:29:56 -0200

    Modificado o redirecionamento da pagina de login

--- calculamedia/settings.py
+++ calculamedia/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,4 +122,5 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
 
-LOGOUT_REDIRECT_URL = 'home'
+# LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = reverse_lazy('cadastro')
